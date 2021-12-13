689a356c5ce117a6cf66da962e972990c7307173
MaudienSmittenaar <57255283+MaudienSmittenaar@users.noreply.github.com>
2021-12-13 14:44:30 +0100

    Fundament opdracht par3.2

--- maudien.py
+++ maudien.py
@@ -1,13 +1,13 @@
 import random
-from bke import EvaluationAgent, start
+from bke import EvaluationAgent, start, can_win
 
 
-
-class MyRandomAgent(EvaluationAgent):
+class MijnSpeler(EvaluationAgent):
     def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1,500)
-    
-    
-my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent, player_x=my_random_agent)
+        getal = 1
+        if can_win(board, opponent_symbol):
+            getal = getal - 1000
+        return getal
 
+mijn_speler = MijnSpeler()
+start(player_o=mijn_speler)
